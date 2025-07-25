a2e97955e9d174b1374decf8eb513eaf4d21d4b5
NipunIITM <24f2006836@ds.study.iitm.ac.in>
2025-07-25 23:55:53 +0530

    Update api_server.py

--- api_server.py
+++ api_server.py
@@ -1,3 +1,10 @@
+import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
+
+
+
 from flask import Flask, jsonify
 import json
 from Scrape_data import fetch_and_parse_data, analyze_data
